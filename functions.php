7fdfc913902d9c62213256bdfe3e7d9a9c738e23
Dien Amina <aminadien94@gmail.com>
2023-04-03 21:41:21 +0200

    Création des pages

--- functions.php
+++ functions.php
@@ -12,3 +12,14 @@ function register_my_menu(){
     register_nav_menu( 'main-menu', 'Menu principal' );
   }
   add_action( 'after_setup_theme', 'register_my_menu' );
+
+  add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
+  function add_extra_item_to_nav_menu( $items, $args ) {
+      if (is_user_logged_in() && $args->menu == 303) {
+          $items .= '<li><a href="'. get_permalink( get_option('woocommerce_myaccount_page_id') ) .'">My Account</a></li>';
+      }
+      elseif (!is_user_logged_in() && $args->menu == 303) {
+          $items .= '<li><a href="' . get_permalink( wc_get_page_id( 'myaccount' ) ) . '">Sign in  /  Register</a></li>';
+      }
+      return $items;
+  }
